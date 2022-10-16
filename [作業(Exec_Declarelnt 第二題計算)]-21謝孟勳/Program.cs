562e78a14cc510f8f15835201a2e52588707e5e0
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:09:14 +0800

    加入專案檔案。

---
+++ [作業(Exec_Declarelnt 第二題計算)]-21謝孟勳/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_Declarelnt_第二題計算___21謝孟勳
+{
+    //求120/7的商以及餘數各是多少
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 120;
+            int Quotient = number / 7;
+            int Remainder = number % 7;
+            Console.WriteLine("商是: " + Quotient);
+            Console.WriteLine("餘數是: " + Remainder); 
+        }
+    }
+}
